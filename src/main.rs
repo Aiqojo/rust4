f94e4282720188fd132e08b4d10f1c2ebf7a0255
Gian <47407561+Aiqojo@users.noreply.github.com>
2022-08-26 18:48:14 -0400

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
-use std::io;
+use std::{cmp::min, io};
 
-const WIDTH: usize = 7;
-const HEIGHT: usize = 6;
+const WIDTH: u8 = 7;
+const HEIGHT: u8 = 6;
 
 fn main() {
-    let mut board: [[u8; WIDTH]; HEIGHT] = [[0; WIDTH]; HEIGHT];
-    let mut winner = 0;
+    let mut board: [[u8; WIDTH as usize]; HEIGHT as usize] = [[0; WIDTH as usize]; HEIGHT as usize];
+    let winner: u8;
     winner = game_loop(&mut board);
     if winner == 1 {
         println!("Player 1 wins!");
@@ -19,7 +19,7 @@ fn main() {
 
 fn game_loop(board: &mut [[u8; 7]; 6]) -> u8 {
     let mut turn: i32 = 1;
-    let mut winner: u8 = 0;
+    let mut winner: u8;
 
     loop {
         let player_move: u8 = get_player_move();
@@ -80,7 +80,7 @@ fn get_player_piece(turn: i32) -> u8 {
 fn get_player_move() -> u8 {
     println!("Enter a column number (1-7): ");
     let mut player_move = String::new();
-    let mut player_move_result: u8 = 0;
+    let player_move_result: u8;
     loop {
         player_move.clear();
         io::stdin()
@@ -140,14 +140,14 @@ fn lowest_row(board: &[[u8; 7]; 6], col: u8) -> u8 {
 
 fn game_over_check(board: &[[u8; 7]; 6], column: u8) -> bool {
     // keep track of how many pieces are in a row
-    let mut win_counter = 1;
+    let mut win_counter: u8 = 0;
     //gets the lowest row of a piece in the column that was selected
     // is one below the lowest 0, which is found by lowest_row()
-    let mut row = lowest_row(board, column) + 1;
+    let row: u8 = lowest_row(board, column) + 1;
     //grabs the color (either 1 or 2) of the piece that was selected
     let color: u8 = board[row as usize][column as usize];
 
-    println!("GOOB row: {row}, column: {column}, color: {color}");
+    //println!("GOOB row: {row}, column: {column}, color: {color}");
 
     // ------------------
     // ----HORIZONTAL----
@@ -159,9 +159,9 @@ fn game_over_check(board: &[[u8; 7]; 6], column: u8) -> bool {
     // you go back the other way and check if there are enough to the left
     // to make a 4 in a row
     //start one offset to the right because we know the first one is correct
-    for i in (column + 1)..WIDTH as u8 {
+    for i in (column + 1)..(WIDTH as u8) {
         if board[row as usize][i as usize] == color {
-            println!("HORIZONTAL: {row}, {i}, {color}");
+            //println!("HORIZONTAL: {row}, {i}, {color}");
             win_counter = win_counter + 1;
         //if its not the same color, break we move to looking to the left
         } else {
@@ -171,13 +171,13 @@ fn game_over_check(board: &[[u8; 7]; 6], column: u8) -> bool {
         if win_counter == 3 {
             return true;
         }
-        println!("HORIZONTAL: win_counter: {win_counter}");
+        //println!("HORIZONTAL: win_counter: {win_counter}");
     }
     //look to the right starting from the original column
     if column > 0 {
-        for i in (0..column - 1).rev() {
+        for i in (0..column).rev() {
             if board[row as usize][i as usize] == color {
-                println!("HORIZONTAL REVERSE: {row}, {i}, {color}");
+                //println!("HORIZONTAL REVERSE: {row}, {i}, {color}");
                 win_counter = win_counter + 1;
             } else {
                 break;
@@ -187,11 +187,122 @@ fn game_over_check(board: &[[u8; 7]; 6], column: u8) -> bool {
                 return true;
             }
         }
+        //println!("HORIZONTAL: win_counter: {win_counter}");
     }
-    // ------------------
+    // ----------------
     // ----VERTICAL----
-    // ------------------
-    win_counter = 1;
+    // ----------------
+    win_counter = 0;
+
+    let mut row_vertical: u8 = min(row + 1, HEIGHT - 1);
+
+    while row_vertical < HEIGHT {
+        if board[row_vertical as usize][column as usize] == color {
+            //println!("VERTICAL: {row_vertical}, {column}, {color}");
+            win_counter = win_counter + 1;
+        } else {
+            break;
+        }
+        row_vertical = row_vertical + 1;
+
+        if win_counter == 3 {
+            return true;
+        }
+    }
+
+    // ----------------
+    // ----DIAGONAL----
+    // ----------------
+
+    // Diagonal down and to the right
+    // Capital X is current piece placed
+    // X
+    //   x
+    //     x
+    //       x
+
+    win_counter = 0;
+
+    // This looks for the furthest you can go eiter down or to the right
+    // and takes the minimum so we dont go out of bounds
+    let furthest_distance_dr = min(HEIGHT - row, WIDTH - column);
+    //println!("furthest_distance: {furthest_distance_dr}");
+    for i in 1..furthest_distance_dr {
+        if board[(row + i) as usize][(column + i) as usize] == color {
+            //println!("DIAGONAL: {row}, {column}, {color}");
+            win_counter = win_counter + 1;
+        } else {
+            break;
+        }
+        if win_counter == 3 {
+            return true;
+        }
+    }
+
+    // Diagonal up and to the left
+    // Capital X is current piece placed
+    //   x
+    //     x
+    //       x
+    //         X
+    win_counter = 0;
+    let furthest_distance_ul = min(row, column);
+    //println!("furthest_distance: {furthest_distance_ul}");
+    for i in 1..furthest_distance_ul {
+        if board[(row - i) as usize][(column - i) as usize] == color {
+            //println!("DIAGONAL: {row}, {column}, {color}");
+            win_counter = win_counter + 1;
+        } else {
+            break;
+        }
+        if win_counter == 3 {
+            return true;
+        }
+    }
+
+    // Diagonal down and to the left
+    // Capital X is current piece placed
+    //       x
+    //     x
+    //   x
+    // X
+
+    win_counter = 0;
+    let furthest_distance_dl = min(HEIGHT - row - 1, column) + 1;
+    //println!("furthest_distance: {furthest_distance_dl}");
+    for i in 1..furthest_distance_dl {
+        if board[(row + i) as usize][(column - i) as usize] == color {
+            //println!("DIAGONAL: {row}, {column}, {color}");
+            win_counter = win_counter + 1;
+        } else {
+            break;
+        }
+        if win_counter == 3 {
+            return true;
+        }
+    }
+
+    // Diagonal up and to the right
+    // Capital X is current piece placed
+    //       x
+    //     x
+    //   x
+    // X
+
+    win_counter = 0;
+    let furthest_distance_ur = min(row, WIDTH - column - 1) + 1;
+    //println!("furthest_distance: {furthest_distance_ur}");
+    for i in 1..furthest_distance_ur {
+        if board[(row - i) as usize][(column + i) as usize] == color {
+            //println!("DIAGONAL: {row}, {column}, {color}");
+            win_counter = win_counter + 1;
+        } else {
+            break;
+        }
+        if win_counter == 3 {
+            return true;
+        }
+    }
 
     return false;
 }
